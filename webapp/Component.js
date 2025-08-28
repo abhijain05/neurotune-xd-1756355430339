59e1d42f1af178b07a3d896834915590eeb36082
abhijain05 <minajain65@gmail.com>
2025-08-28 04:31:08 +0000

    Update from NeuroTune - 2025-08-28T04:31:08.506Z

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,20 +4,27 @@ sap.ui.define([
 ], function (UIComponent, Device) {
   "use strict";
 
+  /**
+   * @class converted.customerfeedbackview.Component
+   * @extends sap.ui.core.UIComponent
+   * Component for the application
+   */
   return UIComponent.extend("converted.customerfeedbackview.Component", {
     metadata: {
       manifest: "json"
     },
 
     /**
-     * The component is initialized by UI5 automatically during the startup of the app
+     * The component is initialized by UI5 automatically during the startup of the app.
+     * @public
+     * @override
      */
     init: function () {
       // Call the base component's init function
       UIComponent.prototype.init.apply(this, arguments);
 
       // Set device model
-      this.setModel(new sap.ui.model.json.JSONModel(sap.ui.Device), "device");
+      this.setModel(new sap.ui.model.json.JSONModel(Device), "device");
 
       // Initialize the router for navigation
       this.getRouter().initialize();
